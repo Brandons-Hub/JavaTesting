1f6f71d15b0d2eb58032546d3573745ab9459754
Brandons-Hub <brandon9oak@gmail.com>
2025-06-24 17:48:53 +0000

    another save

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-document.getElementById('createIceCream').addEventListener('submit',function(event){
+
+
+function strawberry(){
+    let s = document.createElement('p')
+    s.textContent = "There is a strawberry";
+    s.id = "added";
+    document.body.appendChild(s);
+    
+};
+
+
+
+document.getElementById('creatingIceCream').addEventListener('submit',function(event){
     event.preventDefault();
+    let checkedFlavors = document.querySelectorAll('input[name="flavor"]:checked');
+    let flavorValues = Array.from(checkedFlavors).map(f=>f.value);
+    alert(flavorValues.join(', '));
+    let erase = document.getElementById("added");
+    try{
+        erase.remove();
+    } catch{
+
+    };
+    if (flavorValues.includes('strawberry')){
+       strawberry(); 
+    };
 });
+
