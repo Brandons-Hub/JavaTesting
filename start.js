2b1ea80bb93fa7e5340665bec96299788db669d2
Brandons-Hub <brandon9oak@gmail.com>
2025-06-26 06:38:24 +0000

    changing start screen styling

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 function chooseOption(){
+    html.style.filter = "brightness(1.5)";
     startButton.style.top = "-100%";
     startingBackground.style.top = "0";
+    html.style.filter = "brightness(0.90)"
 };
 
 document.getElementById('actualStartButton').addEventListener('click',chooseOption);
