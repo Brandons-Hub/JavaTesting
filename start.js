600d4e3022b2d9395d13ecc02a54b49b88a53348
Brandons-Hub <brandon9oak@gmail.com>
2025-06-26 17:07:52 +0000

    Start screen lookin decent

--- start.js
+++ start.js
@@ -1,8 +1,6 @@
 function chooseOption(){
-    html.style.filter = "brightness(1.5)";
     startButton.style.top = "-100%";
     startingBackground.style.top = "0";
-    html.style.filter = "brightness(0.90)"
 };
 
 document.getElementById('actualStartButton').addEventListener('click',chooseOption);
